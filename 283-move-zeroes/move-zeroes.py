cc4b3b7a31d4494324a36f7431c7104b8360f882
lion31-bit <carl66733@gmail.com>
2026-08-09 16:19:46 +0530

    Time: 11 ms (25.34%) | Memory: 13.6 MB (49.97%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        n=len(nums)
+        i=0
+        for j in range(n):
+            if nums[j]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+        return nums
+
+                
+                    
+
+           
+            
+        
+        
